0cc799f4166df8a8744f778d4380a088c3d0b332
LilibethWitancort <lilibeth.witancort@hotmail.com>
2024-07-14 19:40:15 -0500

    ToolBar component

--- app/src/main/java/pe/idat/appmenucomponents/MenuScreen.kt
+++ app/src/main/java/pe/idat/appmenucomponents/MenuScreen.kt
@@ -12,9 +12,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarDefaults.centerAlignedTopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+
+
+
 
 
 @OptIn(ExperimentalMaterial3Api::class)
